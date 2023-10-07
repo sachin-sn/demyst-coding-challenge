0af3828ca77ead1b246f7dbb416e8c53a7ede41c
sachin-sn <sachinsnagaraj@yahoo.com>
2023-10-07 09:51:53 +0530

    initialization of material UI theme

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//CSS import
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 function App() {
   return <h1>Hello, React!</h1>;
 }
